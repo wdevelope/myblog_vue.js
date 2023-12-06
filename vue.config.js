1667cf235c2cccba49db6dce90e086aca4f2561a
seongwong woo <dnqkr18@gmail.com>
2023-12-06 17:22:06 +0900

    chore: 배포관련 수정

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve('../myblog_backend/src/public'),
+  publicPath: '/',
   devServer: {
-    proxy: process.env.BACKEND_SERVER_PORT,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 });
